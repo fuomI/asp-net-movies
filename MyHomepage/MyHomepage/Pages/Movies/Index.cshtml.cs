0fd328417a766b614766e9cb48ea5484bc37f35f
Joni Jansson <joni.jansson@saunalahti.fi>
2022-07-16 13:21:21 +0300

    Added routing to different items in database, added search function

--- MyHomepage/MyHomepage/Pages/Movies/Index.cshtml.cs
+++ MyHomepage/MyHomepage/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MyHomepage.Data;
 using MyHomepage.Models;
@@ -20,12 +21,35 @@ namespace MyHomepage.Pages.Movies
         }
 
         public IList<Movie> Movie { get;set; } = default!;
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+        public SelectList? Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
             if (_context.Movie != null)
             {
-                Movie = await _context.Movie.ToListAsync();
+                // Use LINQ to get list of genres.
+                IQueryable<string> genreQuery = from m in _context.Movie
+                                                orderby m.Genre
+                                                select m.Genre;
+
+                var movies = from m in _context.Movie
+                             select m;
+
+                if (!string.IsNullOrEmpty(SearchString))
+                {
+                    movies = movies.Where(s => s.Title.Contains(SearchString));
+                }
+
+                if (!string.IsNullOrEmpty(MovieGenre))
+                {
+                    movies = movies.Where(x => x.Genre == MovieGenre);
+                }
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+                Movie = await movies.ToListAsync();
             }
         }
     }
--- MyHomepage/MyHomepage/Program.cs
+++ MyHomepage/MyHomepage/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MyHomepage.Data;
+using MyHomepage.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +10,13 @@ builder.Services.AddDbContext<MyHomepageContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MyHomepageContext") ?? throw new InvalidOperationException("Connection string 'MyHomepageContext' not found.")));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
