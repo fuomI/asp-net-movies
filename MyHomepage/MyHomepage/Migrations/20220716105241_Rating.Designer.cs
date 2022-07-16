78964c72c89b168dc20bb9f8090f1ce20111881d
Joni Jansson <joni.jansson@saunalahti.fi>
2022-07-16 14:30:23 +0300

    Fixed price field, added Rating field

--- MyHomepage/MyHomepage/Migrations/20220716105241_Rating.Designer.cs
+++ MyHomepage/MyHomepage/Migrations/20220716105241_Rating.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyHomepage.Data;
 
@@ -11,9 +12,10 @@ using MyHomepage.Data;
 namespace MyHomepage.Migrations
 {
     [DbContext(typeof(MyHomepageContext))]
-    partial class MyHomepageContextModelSnapshot : ModelSnapshot
+    [Migration("20220716105241_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MyHomepage/MyHomepage/Program.cs
+++ MyHomepage/MyHomepage/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MyHomepage.Data;
 using MyHomepage.Models;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -25,6 +28,15 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+var defaultCulture = new CultureInfo("en-US");
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(defaultCulture),
+    SupportedCultures = new List<CultureInfo> { defaultCulture },
+    SupportedUICultures = new List<CultureInfo> { defaultCulture }
+};
+app.UseRequestLocalization(localizationOptions);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
