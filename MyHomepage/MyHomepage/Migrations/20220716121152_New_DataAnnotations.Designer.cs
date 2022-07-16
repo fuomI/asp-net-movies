2d18728ff15a96d102cbb5d414945520a3cfe75c
Joni Jansson <joni.jansson@saunalahti.fi>
2022-07-16 15:16:49 +0300

    updated data annotations in movie model

--- MyHomepage/MyHomepage/Migrations/20220716121152_New_DataAnnotations.Designer.cs
+++ MyHomepage/MyHomepage/Migrations/20220716121152_New_DataAnnotations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyHomepage.Data;
 
@@ -11,9 +12,10 @@ using MyHomepage.Data;
 namespace MyHomepage.Migrations
 {
     [DbContext(typeof(MyHomepageContext))]
-    partial class MyHomepageContextModelSnapshot : ModelSnapshot
+    [Migration("20220716121152_New_DataAnnotations")]
+    partial class New_DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
