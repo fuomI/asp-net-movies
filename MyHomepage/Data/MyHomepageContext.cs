7b53b7fd084d0286e33da8447726763c43786d6c
Joni Jansson <joni.jansson@saunalahti.fi>
2022-07-16 15:35:27 +0300

    restructuring project

---
+++ MyHomepage/Data/MyHomepageContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyHomepage.Models;
+
+namespace MyHomepage.Data
+{
+    public class MyHomepageContext : DbContext
+    {
+        public MyHomepageContext (DbContextOptions<MyHomepageContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyHomepage.Models.Movie>? Movie { get; set; }
+    }
+}
